b2fea90895ac348e94a05f7dbee509d4d1a4d3dd
AnujKarmacharya <karmacharyaanuj27@gmail.com>
2024-06-23 15:36:52 +0545

    test

--- decorator.py
+++ decorator.py
@@ -8,13 +8,16 @@
 
 def star(func):
     def wrapper():
-        print("*"*10)
+        print("*" * 10)
         func()
-        print("*"*10)
+        print("*" * 10)
+
     return wrapper
+
 
 @star
 def hello():
     print("hello")
+
 
 hello()
